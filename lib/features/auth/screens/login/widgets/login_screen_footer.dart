80530f8434be77bfea75a853c650a33d29c056c4
Sobhy <sobhy.abdelhakam25@gmail.com>
2024-11-12 00:01:06 +0200

    complete login screen

--- lib/features/auth/screens/login/widgets/login_screen_footer.dart
+++ lib/features/auth/screens/login/widgets/login_screen_footer.dart
@@ -1,3 +1,7 @@
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreenFooter extends StatelessWidget {
@@ -7,25 +11,69 @@ class LoginScreenFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        const Expanded(
-            child: Divider(
-          thickness: 0.5,
-          indent: 40,
-          endIndent: 10,
-        )),
-        Text(
-          'Or Log in with',
-          style: Theme.of(context).textTheme.labelMedium,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Flexible(
+                child: Divider(
+              thickness: 0.5,
+              indent: 40,
+              endIndent: 10,
+            )),
+            Text(
+              TTexts.orSignInWith,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const Flexible(
+              child: Divider(
+                thickness: 0.5,
+                indent: 10,
+                endIndent: 40,
+              ),
+            ),
+          ],
         ),
-        const Expanded(
-            child: Divider(
-          thickness: 0.5,
-          indent: 10,
-          endIndent: 40,
-        )),
+        const SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: TColors.grey, width: 2),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Image(
+                  width: TSizes.iconMd,
+                  height: TSizes.iconMd,
+                  image: AssetImage(TImages.google),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: TSizes.spaceBtwItems,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: TColors.grey, width: 2),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Image(
+                  width: TSizes.iconMd,
+                  height: TSizes.iconMd,
+                  image: AssetImage(TImages.facebook),
+                ),
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
--- lib/features/auth/screens/login/widgets/login_screen_header.dart
+++ lib/features/auth/screens/login/widgets/login_screen_header.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
@@ -9,13 +10,14 @@ class LoginScreenHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = Theme.of(context).brightness == Brightness.dark;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Image(
+        Image(
           height: 150,
           fit: BoxFit.cover,
-          image: AssetImage('assets/logos/shopping.png'),
+          image: AssetImage(dark ? TImages.lightAppLogo : TImages.darkAppLogo),
         ),
         Text(
           TTexts.loginTitle,
